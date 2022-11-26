0c3d3c61d89535902d38c66042a68934a8a5ece5
Jorge Díez de la Fuente <buker@stuker.es>
2022-11-26 20:32:13 +0100

    telemetry metric: contract

--- telemetry/metrics.go
+++ telemetry/metrics.go
@@ -13,6 +13,11 @@ var Metrics struct {
 		Total    func(labels.Contract) prometheus.Counter    `name:"count" help:"Total number of TCP connections"`
 		Lifetime func() TimeHistogram                        `name:"lifetime_seconds" help:"Connection lifetime" buckets:".1,1,5,15,30,60,90,120"`
 	} `namespace:"connection"`
+	Contract struct {
+		Enrolled  func(labels.Contract) prometheus.Gauge      `name:"enrollment_count" help:"Contract enrollment"`
+		Error     func(labels.ContractErr) prometheus.Counter `name:"error_count" help:"Number of contract enrollment errors"`
+		Heartbeat func(labels.ContractErr) prometheus.Counter `name:"heartbeat_count" help:"Number of heartbeats sent"`
+	} `namespace:"contract"`
 	Net struct {
 		RemainingCapBytes func(labels.ContractNetCap) prometheus.Gauge `name:"remaining_caplimit_bytes" help:"Remaining capacity bytes"`
 		CapLimitsBytes    func(labels.ContractNetCap) prometheus.Gauge `name:"caplimit_bytes" help:"Capacity threshold bytes"`
